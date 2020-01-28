97858310c4873667f80fc9bf69dce766e42a4326
juhu1705 <Fabius1705@live.de>
2020-01-28 14:57:00 +0100

    Commit

--- ModellTrainController/src/de/noisruker/client/gui/GUIClient.java
+++ ModellTrainController/src/de/noisruker/client/gui/GUIClient.java
@@ -10,11 +10,15 @@ import de.noisruker.common.messages.ChatMessage;
 import de.noisruker.net.datapackets.Datapacket;
 import de.noisruker.net.datapackets.DatapacketType;
 import de.noisruker.util.Ref;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.SplitPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class GUIClient implements Initializable {
 
@@ -25,7 +29,13 @@ public class GUIClient implements Initializable {
 	}
 
 	@FXML
+	public SplitPane setTo;
+
+	@FXML
 	public TextArea messages;
+
+	@FXML
+	public ImageView stream;
 
 	@FXML
 	public TextField send;
@@ -43,6 +53,29 @@ public class GUIClient implements Initializable {
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		instance = this;
+
+		new Thread(() -> {
+			String imageSource = "http://192.168.0.172:8080/";
+
+			while (true) {
+
+				Image img;
+
+				Ref.LOGGER.info((img = new Image(imageSource, true)).toString());
+
+				Ref.LOGGER.info(img.getWidth() + "");
+				Ref.LOGGER.info(img.isError() + "");
+				Platform.runLater(() -> {
+					stream.setImage(img);
+				});
+
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
 	}
 
 }
