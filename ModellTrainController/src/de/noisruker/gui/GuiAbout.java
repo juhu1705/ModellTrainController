d6611edb3d386bbd05c063551704233dc5a5a7b2
juhu1705 <Fabius1705@live.de>
2021-07-11 23:16:57 +0200

    Add PGP Key to About window

--- ModellTrainController/src/de/noisruker/gui/GuiAbout.java
+++ ModellTrainController/src/de/noisruker/gui/GuiAbout.java
@@ -1,17 +1,38 @@
 package de.noisruker.gui;
 
+import de.noisruker.main.GUILoader;
 import de.noisruker.util.Ref;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import org.controlsfx.control.Notifications;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
 
 public class GuiAbout implements Initializable {
 
     @FXML
     public Label version;
+
+    public void onPgPKey(ActionEvent event) {
+        try {
+            if(Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE))
+                Desktop.getDesktop().browse(new URI("https://keys.openpgp.org/search?q=fabius.mettner%40gmx.de"));
+            else {
+                Ref.LOGGER.log(Level.SEVERE, "Can not browse link! Here the link to the pgp key: https://keys.openpgp.org/search?q=fabius.mettner%40gmx.de");
+                Notifications.create().darkStyle().title("Can not browse link!").text("Here the link to the pgp key: https://keys.openpgp.org/search?q=fabius.mettner%40gmx.de").owner(GUILoader.getPrimaryStage()).showConfirm();
+            }
+        } catch (IOException | URISyntaxException | UnsupportedOperationException e) {
+            Ref.LOGGER.log(Level.SEVERE, "Can not browse link! Here the link to the pgp key: https://keys.openpgp.org/search?q=fabius.mettner%40gmx.de");
+        }
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
